526201bc0dcede3817b0b41007ec43001b2d2c2c
hilarious559 <hilryaura@gmail.com>
2025-08-28 02:05:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO~89f58f2b562963d778c290889cd880a0ab3a8a34", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254741405899", // Put Owner Number Here 
+  PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
