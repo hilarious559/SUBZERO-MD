323daaa6a95f3be9a6199207816cf10beafb5e6d
hilarious559 <hilryaura@gmail.com>
2025-08-28 09:22:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~89f58f2b562963d778c290889cd880a0ab3a8a34", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~6578088c239d02f26868b2a3de6d434856ccee10", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254741405899", // Put Owner Number Here 
   PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
