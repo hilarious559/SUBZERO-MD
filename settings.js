f42cec4a9e80418253b769a279f35731008c14d1
hilarious559 <hilryaura@gmail.com>
2025-10-09 07:10:30 +0300

    Update settings.js for tere

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~6578088c239d02f26868b2a3de6d434856ccee10", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "254741405899", // Put Owner Number Here 
-  PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~cb6c1821fe34865f743956e981152a6367e65926",
+  OWNER_NUMBER: "+254741405899", 
+  PREFIX: ",",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
